91faccc8c16af268dea02096d382d1cf433c2a93
angelstchavez <angelstchavez@gmail.com>
2023-09-17 14:52:02 -0500

    Add pagination in findAll method in to pokemon

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -20,7 +20,7 @@ export class SeedService {
     await this.pokemonModel.deleteMany({});
 
     const data = await this.http.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=1000',
     );
 
     const pokemonToInsert: { name: string; no: number }[] = [];
